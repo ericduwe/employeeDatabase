bbd5e76a3625dc17c894d446d04c828ee571e39d
Eric Duwe <ericduwe@gmail.com>
2021-09-07 23:36:04 -0500

    added tables to schema.sql

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util')
 const writeFileAsync = util.promisify(fs.writeFile)
+
+const promptUser = () => {
+    return inquirer.prompt([
+
+        {
+        type: 'list',
+        name: 'question',
+        message: "What would you like to do?",
+        choices: ["view all departments", "view all roles", "view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+        },
+
+    ]);
+};
