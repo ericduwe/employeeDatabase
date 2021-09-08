3b539e8b9aba6e3745aaa0dc01688e87b10b4768
Eric Duwe <ericduwe@gmail.com>
2021-09-07 23:42:02 -0500

    inserted values into department table in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO department (id, dept_name)
+VALUES ( 001, "Sales"),
+       ( 002, "Marketing"),
+       ( 003, "Development"),
+       ( 004, "Management"),
+       ( 005, "Quality Control");
